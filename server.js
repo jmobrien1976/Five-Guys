a6149120ed768d7b1ce131ca276cf65574ec8d04
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-09-14 17:02:35 -0600

    added that utils folder, uncommented some stuff on the front end, and modified the server.js file slightly

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const calcOrderAmount = (items) => {
   //do calculation of total order here
@@ -38,6 +39,7 @@ app.post("/create-payment-intent", async (req, res) => {
 
 const sess = {
   secret: 'Super secret secret',
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -47,7 +49,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
