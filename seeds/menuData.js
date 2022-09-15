f0199a6d3ba0c11cfd3b06f2b604ee842e6bfd6f
Gabe Vee <0gabevee0@gmail.com>
2022-09-15 14:06:48 -0600

    updated seeding data code

--- seeds/menuData.js
+++ seeds/menuData.js
@@ -1,35 +1,33 @@
-const { Menu_items } = require('../models');
+const { Menu_items } = require("../models");
 
-const menudata = [ 
-    {
-    "name": "hambuger",
-    "description": "meat and bacon",
-    "price": "9.99"
-    },
-    {
-      "name": "cheese burger",
-      "description": "cheese with meat",
-      "price": "7.99"
-    },
-    {
-        "name": "chicken burger",
-        "description": "fried chicken with pickle and lettuce",
-        "price": "8.99"
-    },
-    {
-        "name": "french fries",
-        "description": "classic french fries",
-        "price": "2.99"
-    },
-    {
-        "name": "soda ",
-        "description": "coke and sprite",
-        "price": "1.99"
-    }
-   
-  ];
+const menudata = [
+  {
+    name: "hambuger",
+    description: "meat and bacon",
+    price: "9.99",
+  },
+  {
+    name: "cheese burger",
+    description: "cheese with meat",
+    price: "7.99",
+  },
+  {
+    name: "chicken burger",
+    description: "fried chicken with pickle and lettuce",
+    price: "8.99",
+  },
+  {
+    name: "french fries",
+    description: "classic french fries",
+    price: "2.99",
+  },
+  {
+    name: "soda ",
+    description: "coke and sprite",
+    price: "1.99",
+  },
+];
 
-const seedMenu = () => {
-    Menu_items.bulkCreate(menudata);}
+const seedMenu = () => Menu_items.bulkCreate(menudata);
 
 module.exports = seedMenu;
